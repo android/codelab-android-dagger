671cbe9c6061d6d635ec81240b892ca17d8f91f9
Giuseppe Filograno <filograno@gmail.com>
2020-05-01 19:14:09 +0200

    Add SplashActivity

---
+++ app/src/main/java/com/example/android/dagger/splash/SplashActivity.kt
@@ -0,0 +1,11 @@
+package com.example.android.dagger.splash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
