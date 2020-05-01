d54555100feca7eba61a736969b41f1c7c3fe1e8
Giuseppe Filograno <filograno@gmail.com>
2020-05-01 19:29:31 +0200

    Add injection for SplashActivity and SplashViewModel

--- app/src/main/java/com/example/android/dagger/splash/SplashActivity.kt
+++ app/src/main/java/com/example/android/dagger/splash/SplashActivity.kt
@@ -2,10 +2,20 @@ package com.example.android.dagger.splash
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.android.dagger.MyApplication
+import javax.inject.Inject
 
 class SplashActivity : AppCompatActivity() {
 
+    // @Inject annotated fields will be provided by Dagger
+    @Inject
+    lateinit var splashViewModel: SplashViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        // Creates an instance of Splash component by grabbing the factory from the app graph
+        // and injects this activity to that Component
+        (application as MyApplication).appComponent.splashComponent().create().inject(this)
         super.onCreate(savedInstanceState)
     }
 }
