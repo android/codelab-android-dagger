0e57ccb801c02b5faafab28c5fe610380d8cc4a7
WhoisAbel <whoisabel1996@gmail.com>
2022-07-11 12:27:15 +0430

    1-@Inject_annotation
    - Start refactoring the Registration flow to use Dagger.

--- app/src/main/java/com/example/android/dagger/registration/RegistrationViewModel.kt
+++ app/src/main/java/com/example/android/dagger/registration/RegistrationViewModel.kt
@@ -17,12 +17,13 @@
 package com.example.android.dagger.registration
 
 import com.example.android.dagger.user.UserManager
+import javax.inject.Inject
 
 /**
  * RegistrationViewModel is the ViewModel that the Registration flow ([RegistrationActivity]
  * and fragments) uses to keep user's input data.
  */
-class RegistrationViewModel(val userManager: UserManager) {
+class RegistrationViewModel @Inject constructor(val userManager: UserManager) {
 
     private var username: String? = null
     private var password: String? = null
